8e41f2a6bc6d7de9b13ae71198519291f35b3a22
Murphey Osmundson <mosmundson88@gmail.com>
2023-05-31 08:33:35 -0400

    add functional ingredient building steps.

--- recipes/admin.py
+++ recipes/admin.py
@@ -2,11 +2,27 @@ from django.contrib import admin
 
 # Register your models here.
 from django.contrib import admin
-from recipes.models import Recipe
+from recipes.models import Recipe, RecipeStep, Ingredient
 
 @admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
     list_display = (
         "title",
+        "id",
+    )
+
+@admin.register(RecipeStep)
+class RecipeStepAdmin(admin.ModelAdmin):
+    list_display = (
+        "step_number",
+        "instruction",
+        "id",
+    )
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = (
+        "amount",
+        "food_item",
         "id",
     )
